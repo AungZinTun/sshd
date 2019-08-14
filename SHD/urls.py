1d473c9910e18c32ac1206ca4f6ad6f49be9cc88
AungZinTun <aungzintun2@gmail.com>
2019-08-15 05:14:44 +0630

    dj

--- SHD/urls.py
+++ SHD/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.dashboard, name='dashboard'),
-    path('tb', include('TB.urls'))
+    # path('tb', include('TB.urls'))
 ]
